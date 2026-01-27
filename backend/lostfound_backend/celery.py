8f0e4d2e91b4fe5b234371115ee9916ebde18366
ADMIN <admin@MacBook-Air-3.local>
2026-01-27 20:48:40 +0200

    update : celery hardcoded configuration on setting

--- backend/lostfound_backend/celery.py
+++ backend/lostfound_backend/celery.py
@@ -7,6 +7,4 @@ app = Celery("lostfound_backend")
 
 # Load task modules from all registered Django apps
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()  # <-- this tells Celery to find tasks.py in apps
-
-app.conf.result_backend = "redis://redis:6379/1"
+app.autodiscover_tasks()
