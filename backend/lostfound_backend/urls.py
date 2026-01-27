5b3ccf1bc562a23e7493ce5d7477b6148ac519d9
ADMIN <admin@MacBook-Air-3.local>
2026-01-27 23:31:23 +0200

    Fix media url in settings

--- backend/lostfound_backend/urls.py
+++ backend/lostfound_backend/urls.py
@@ -2,10 +2,12 @@
 URL configuration for lostfound_backend project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.views.static import serve
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+import os
 
 def health_check(request):
     return HttpResponse("OK", content_type="text/plain")
@@ -18,5 +20,6 @@ urlpatterns = [
 ]
 
 # Serve static and media files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
